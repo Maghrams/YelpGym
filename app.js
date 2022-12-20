88be7a1fa15cc7223cc8663167c525d6b7eec350
Maghrams <fmughram@stu.kau.edu.sa>
2022-12-20 14:18:58 +0300

    added these files for the meeting 20/10/2022 to be elaborated

---
+++ app.js
@@ -0,0 +1,31 @@
+// const express = require('express');
+// //const path = require('path');
+// const mongoose = require('mongoose');
+// const ejsMate = require('ejs-mate');
+// const { GymSchema } = require('./schemas.js');
+// //const catchAsync = require('./utils/catchAsync');
+// //const ExpressError = require('./utils/ExpressError');
+// const methodOverride = require('method-override');
+// const Gym = require('./models/gym');
+//
+//
+// //
+// //
+// //
+// //
+// //
+// //
+// //
+// //
+// // const app = express();
+// // //app.engine('ejs', engine);
+// //
+// // app.set('views', __dirname + '/views');
+// // app.set('view engine', 'ejs'); // so you can render('index')
+// //
+// // // render 'index' into 'boilerplate':
+// // app.get('/',function(req,res,next){
+// //     res.render('index', { what: 'best', who: 'me' });
+// // });
+// //
+// // app.listen(3000);

28b84aff9eb2d032b33a68fc241bbca62f09599d
Maghrams <fmughram@stu.kau.edu.sa>
2023-01-12 13:33:57 +0300

    Deleted console.log test

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Gym = require('./models/gym');
+const GymModel = require('./models/gym');
 const { v4: uuidv4 } = require('uuid');
 
-console.log(uuidv4())
 //localhost:27017 doesn't work because it is not configured properly , so 127.0.0.1:27017 was used
 mongoose.connect('mongodb://127.0.0.1:27017/YelpGym_Database',{
     //Default options with mongoDB connection
@@ -27,9 +26,9 @@ app.get('/',(req, res) =>{
     res.render('index');
 })
 
-//TODO delete test case
+
 app.get('/newgym', async (req, res) =>{
-    const gym = new Gym.Gym({
+    const gym = new GymModel.Gym({
         gymUUID:uuidv4(),
         name:"Test Gym",
         addrress:{
@@ -37,7 +36,8 @@ app.get('/newgym', async (req, res) =>{
             contactNumber:'+966546400149',
             location:['_21.52530477062381','_39.190138604320246'] //FIXME numbers dont appear in DB, check why .
         },
-        reviews:[],
+        reviews:[
+        ],
         owner:{
             name:'Faisal',
             phoneNumber:'+966536330154'
@@ -46,6 +46,7 @@ app.get('/newgym', async (req, res) =>{
     await gym.save();
     res.send(gym);
 })
+
 app.listen(3000, ()=>{
     console.log('Serving on port 3000');
 })

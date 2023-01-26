e8326bc36b507f2eb97b4b2a226e6f930fdffc4e
AmjdIssa <aissa0012@stu.kau.edu.sa>
2023-01-26 15:09:15 +0300

    delete console.log

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/YelpGym_Database',{
     useUnifiedTopology: true
 });
 
-console.log()
-console.log()
-console.log()
-
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"connection error"));
 db.once("open",()=>{

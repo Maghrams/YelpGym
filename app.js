4a1db798f625f3c746e390d31d3df21d21cb8ed2
poweruser <ketcats@gmail.com>
2023-04-21 18:09:01 +0300

    Used passport, &passport local to authenticate user logins

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 require('dotenv').config()
-const express = require('express'); //BAck-end framework, used to create server
-const path = require('path');
+const express = require('express'); //Back-end framework, used to create server
+const path = require('path');//Path module to get the path of the current directory, make it works with different OSes
 const mongoose = require('mongoose'); //sMongoDB connections, schema and model (ORM Approach), and query builder
 const ejsMate =  require('ejs-mate') // Template engine for ejs,
 const methodOverride = require('method-override');//Method override function to fix the problem of not being able to use PUT and DELETE methods
 const morgan = require('morgan');//DEV info to check API requests
 const session = require('express-session');//Session middleware to store user data
 const flash = require('connect-flash');//Flash middleware to send messages to the user
+const passport = require('passport');//Passport middleware to authenticate users
+const LocalStrategy = require('passport-local');//Local Strategy to authenticate users with username and password
+const User = require('./models/user');//Import User model
+
+
+//Import Routes
 const gymRoutes = require('./routes/gyms');//Import gym routes
 const reviewRoutes = require('./routes/reviews');//Import review routes
-
+const userRoutes = require('./routes/users');//Import user routes
 
 //Initialize Connection to MongoDB with parameters
 mongoose.connect(process.env.CONNECTION_STRING, {
@@ -49,9 +55,15 @@ const sessionConfig = {
         maxAge: 1000*60*60*24*7
     }
 }
-app.use(session(sessionConfig))
-app.use(flash())
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
+//These two methods are provided by passport-local-mongoose for session storing and destroying
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 //Custom Middleware to send flash messages to the user
 app.use((req, res, next)=>{
     res.locals.success = req.flash('success');
@@ -59,9 +71,12 @@ app.use((req, res, next)=>{
     next();
 })
 
+
+
 //Routes
 app.use('/gyms',gymRoutes);
 app.use('/gyms/:id/reviews',reviewRoutes);
+app.use('/',userRoutes);
 
 //On GET root directory request => render home page
 app.get('/',(req, res) =>{
@@ -71,9 +86,9 @@ app.get('/',(req, res) =>{
 //TODO Apply page not found for non-existing gym IDs and { /gyms/* } pages
 //If Page doesnt exist, render not_found page and send 404 status code
 
-app.all('*',(req, res,next)=>{
-    res.status(404).render("not_found")
-})
+// app.all('*',(req, res,next)=>{
+//     res.status(404).render("not_found")
+// })
 
 //Error Middleware if any error occur from catchAsync function send it here,
 //It's either a predefined error that has a predefined error message and status code

8e5a3d1e190dbdde07eb09d21ab5991b36b8c86a
poweruser <ketcats@gmail.com>
2023-04-21 18:14:25 +0300

    Passport package will manage username and password authentication and cxhecking in DB so it is suggested to remove them form the users Schema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const userSchema = new Schema({
-    name: String,
+    //Added by Passport Plugin
+    // name: {
+    //     type: String,
+    //     required: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     reviews: [{
-        gymUUID: String,
-        rating: Number,
-        comment: String
+        gymID: String,
+        body: String,
+        rating: Number
     }]
 });
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
